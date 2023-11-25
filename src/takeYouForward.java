16fa146c7630fb50ac18424c9679a07f91333532
Gaurav <gauravnath725@gmail.com>
2023-11-25 16:17:36 +0530

    changes

--- src/takeYouForward.java
+++ src/takeYouForward.java
@@ -2,12 +2,14 @@ import java.util.Arrays;
 
 public class takeYouForward {
     public static void main(String[] args){
-       int arr1[] = {2,5,3,6,4,7};
+    //    int arr1[] = {2,5,3,6,4,7};
     //    System.out.println(sort(arr1));
-       int[] arr2 = {2,63,6,7,334,7,2};
+    //    int[] arr2 = {2,63,6,7,334,7,2};
     //    System.out.println(sort(arr2));
-    System.out.println(findLargestElement(arr1));
-    System.out.println(findLargestElement(arr2));
+    // System.out.println(findLargestElement(arr1));
+    // System.out.println(findLargestElement(arr2));
+    int arr[] = {1,2,7,4,5},n=5;
+    System.out.println(isSorted(arr, n));
     }    
     static int sort(int[] arr){
         Arrays.sort(arr);
@@ -21,5 +23,12 @@ public class takeYouForward {
             }
         }
         return max;
+    }
+    static boolean isSorted(int[] arr, int n){
+        for(int i=1;i<n;i++){
+            if(arr[i]<arr[i-1])
+            return false;
+        }
+        return true;
     }
 }
